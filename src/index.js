05d25150723f3559f870670e6c77e503ae919dc3
GeraldLuntungan0 <124539241+GeraldLuntungan0@users.noreply.github.com>
2023-05-13 01:03:12 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import "tailwindcss/tailwind.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
-import AboutUs from "./pages/AboutUs";
+import AboutUs from "../src/pages/aboutus";
 import Diets from "./pages/Diets";
 import Workout from "./pages/Workout";
-import Card from "./components/card";
+import DietAtkins from "./pages/DietAtkins";
+import DietZone from "./pages/DietZone";
+import DietVegan from "./pages/DietVegan";
+import DietIntermiten from "./pages/DietIntermiten";
+import DietMediterania from "./pages/DietMediterania";
 
 const router = createBrowserRouter([
   {
@@ -25,6 +29,26 @@ const router = createBrowserRouter([
   {
     path: "/aboutus",
     element: <AboutUs />,
+  },
+  {
+    path: "/dietatkins",
+    element: <DietAtkins />,
+  },
+  {
+    path: "/dietzone",
+    element: <DietZone />,
+  },
+  {
+    path: "/dietvegan",
+    element: <DietVegan />,
+  },
+  {
+    path: "/dietintermiten",
+    element: <DietIntermiten />,
+  },
+  {
+    path: "/dietmediterania",
+    element: <DietMediterania />,
   },
 ]);
 
